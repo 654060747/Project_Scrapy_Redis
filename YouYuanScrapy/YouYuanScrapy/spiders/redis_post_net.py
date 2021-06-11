96a94b2b6359daf2875d8edabffd71e1a79fdf49
654060747 <654060747@qq.com>
2021-06-11 17:39:57 +0800

    scrapy-redis post请求

---
+++ YouYuanScrapy/YouYuanScrapy/spiders/redis_post_net.py
@@ -0,0 +1,38 @@
+import scrapy,json
+from scrapy_redis.spiders import RedisSpider
+
+#如果想要爬虫在一开始的时候就发送post请求,那么就需要在爬虫类中重写start_requests(self)方法,
+#并且不再调用start_urls里面的url,如果不重写start_requests(self)那么爬虫会自动获取start_urls里面的url
+
+class PostSpider(RedisSpider):
+    name = 'redis_post'
+    allowed_domains = ['192.168.40.88:5000']
+    # 重写start_requests方法默认不调用
+    # start_urls = ['http://192.168.40.88:5000/test1']
+    redis_key = 'post:start_urls'
+
+    # 方法重写，也可以在此方法中获取动态cookie等
+    def make_request_from_data(self,data): # data数据即为post:start_urls发送过来的数据，不过为bytes类型
+        print("\n"*10)
+        print("=========="+data.decode())
+        url = data.decode()
+        formdata = {
+            "name": "狗",
+            "age": "AUTO",
+            "age": "AUTO",
+        }
+        # Scrapy分布式爬虫过滤问题,不会显示更多重复项
+        # 报错：DEBUG:Filtered duplicate request:<GET:xxxx>-no more duplicates will be shown
+        # 原因：传入的网址是已经爬取过的，判断为已经爬取过的，程序自动结束
+        # yield scrapy.Request(xxxurl,callback=self.xxxx)中有重复的请求
+        # 要想爬虫再次运行：可以在Request中添加dont_filter=True(强制不过滤)
+        # scrapy-redis发送post请求，重写方法make_request_from_data，这里必须return
+        return scrapy.FormRequest(url=url, formdata=formdata, callback=self.parse, dont_filter=True)
+
+
+    def parse(self, response):
+        print('\n'*3)
+        print('*'*20)
+        print(response.text)
+        print('*'*20)
+        print('\n'*3)
